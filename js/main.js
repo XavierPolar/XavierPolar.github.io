6a2769cf5530e3d3b76ea1e02aab90ce08c2af94
Xavier Polar <xavigarixx@gmail.com>
2021-03-02 13:10:18 -0600

    Lo logramos cambiaos ese pequeño cuadro de Ajax

--- js/main.js
+++ js/main.js
@@ -2,16 +2,18 @@ $(document).ready(function(){
 
 // Cambiar el texto 
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  var peticion = new XMLHttpRequest();
+  url = 'http://polarxavier.me/text/info.html';
+  peticion.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("texto_cambio").innerHTML =
       this.responseText;
     }
   };
-  xhttp.open("GET", "..text/info.txt", true);
-  xhttp.send();
+  peticion.open("GET", url, true);
+  peticion.send();
 }
+document.getElementById("id_ajax").addEventListener("click", function(){loadDoc();});
 
 // Calcular el tamaño de la pantalla
 var w = window.innerWidth
