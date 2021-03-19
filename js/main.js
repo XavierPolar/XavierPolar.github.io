a505ac97f7398fba3e1fdea3935073b77790cfd2
Xavier Polar <xavigarixx@gmail.com>
2021-03-19 12:10:24 -0600

    cambios al parecer menores pero mas cute todo

--- js/main.js
+++ js/main.js
@@ -19,22 +19,33 @@ function loadDoc() {
 }
 document.getElementById("id_ajax").addEventListener("click", function(){loadDoc();});
 
+let sugerencias
+(function($){
+  sugerencias = (sugerencias) =>{
+    var peticion;
+    url = "http://polarxavier.me/text/lista.php?q=";
+    x= document.getElementsById("txt1");
+    x.value = str;
+    console.log(x);
+    console.log( document.getElementsById("txt1"));
+    if (str.length == 0){
+      document.getElementsById("sugerencias").innerHTML = "";
+      return;
+    }
+    peticion = new XMLHttpRequest();
+    peticion.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200) 
+        document.getElementsById("sugerencia").innerHTML = this.responseText;
+    };
+    peticion.open("GET", ulr+str, true);
+    peticion.send();
 
-function mostrar_sugerencias(str){
-  var peticion;
-  url = 'http://polarxavier.me/text/lista.php?q=';
-  if (str.length == 0){
-    document.getElementsById("sugerencia").innerHTML = "";
-    return;
-  }
-  peticion = new XMLHttpRequest();
-  peticion.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) 
-      document.getElementsById("sugerencia").innerHTML = this.responseText;
-  };
-  peticion.open("GET", url+str, true);
-  peticion.send();
-}
+   }
+})(jQuery)
+
+  
+
+// document.getElementById("sugerencia").addEventListener("keyup", function(){mostrar_sugerencias(str);});
 
 // Calcular el tamaño de la pantalla
 var w = window.innerWidth
